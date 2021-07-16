ff4ee5e7ccce4c9f10ccb844d642587ab481605a
kimurloc <andynamgoo@gmail.com>
2021-07-16 11:07:23 +0900

    asdf

--- app/src/main/java/com/example/a2021summer/MainActivity.kt
+++ app/src/main/java/com/example/a2021summer/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.a2021summer
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.a2021summer.databinding.ActivityMainBinding
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -11,12 +11,19 @@ import java.net.URL
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        btnRequest.setOnClickListener{
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = viewBinding.root
+        setContentView(view)
+
+
+        viewBinding.btnRequest.setOnClickListener{
             thread(start=true){//스레드로 시작
-                val urlText = "http://118.45.175.171:14766/byeongseong/index.jsp".toString()
+                val urlText = "http://172.30.1.31:14766/byeongseong/makenew.jsp".toString()
                 val url = URL(urlText)//url 객체 생성
                 val urlConnection = url.openConnection() as HttpURLConnection//openConnection으로 서버와 연결, HttpURLConnection으로 변환
                 if(urlConnection.responseCode == HttpURLConnection.HTTP_OK){//응답이 괜찮으면
@@ -31,7 +38,7 @@ class MainActivity : AppCompatActivity() {
                     buffered.close()
                     urlConnection.disconnect()
                     runOnUiThread{
-                        textView.text = content.toString()
+                        viewBinding.textView.text = content.toString()
                     }
                 }
             }
